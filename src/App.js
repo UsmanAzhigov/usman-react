ad1bc6d95bd60b8941ae3dde7784ae57a10b1153
Archakov Dennis <hello@archakov.im>
2022-09-28 22:05:58 +0600

    Сайт авторизации

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import { FullPost } from './pages/FullArticle';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Login } from './pages/Login';
-import { Profile } from './components/Profile';
-import { NotFound } from './pages/NotFound';
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 export default function App() {
+  const [fields, setFields] = React.useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleClickClear = () => {
+    setFields({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
+  };
+
+  const handleClickRegistred = () => {
+    if (!fields.email.includes('@')) {
+      alert('Почта неверная!');
+      return;
+    }
+    if (fields.firstName.lenght < 3 || fields.lastName.length < 3) {
+      alert('Имя или фамилия указаны неверно!');
+      return;
+    }
+    if (fields.passwords.lenght < 6) {
+      alert('Пароль должен содержать 6 символов или больше!');
+      return;
+    }
+    console.log('ЗАРЕГИСТРИРОВАЛИСЬ', fields);
+    handleClickClear();
+  };
+
+  const handleChangInput = (event) => {
+    const { name, value } = event.target.name;
+    setFields({
+      ...fields,
+      [name]: value,
+    });
+  };
+  const isValid = fields.firstName && fields.lastName && fields.email && fields.password;
+  console.log(isValid);
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/post/:id" element={<FullPost />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<NotFound />}></Route>
-        </Route>
-      </Routes>
-    </>
+    <div className="App">
+      <div>
+        <TextField
+          name="firstName"
+          onChange={handleChangInput}
+          id="standard-basic"
+          label="Имя"
+          variant="standard"
+          value={fields.firstName}
+        />
+        <TextField
+          name="lastName"
+          onChange={handleChangInput}
+          id="standard-basic"
+          label="Фамилия"
+          variant="standard"
+          value={fields.lastName}
+        />
+      </div>
+      <div>
+        <TextField
+          name="email"
+          onChange={handleChangInput}
+          id="standard-basic"
+          label="Email"
+          variant="standard"
+          value={fields.email}
+        />
+        <TextField
+          name="password"
+          onChange={handleChangInput}
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          value={fields.password}
+        />
+      </div>
+      <br />
+      <Button
+        disabled={!isValid}
+        onClick={handleClickRegistred}
+        className="btn"
+        variant="contained">
+        Зарегистрироваться
+      </Button>
+      <Button onClick={handleClickClear} variant="outlined" color="error">
+        Очистить
+      </Button>
+    </div>
   );
 }
--- src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <div>
-      <h3>404 not found</h3>
-    </div>
-  );
-};
