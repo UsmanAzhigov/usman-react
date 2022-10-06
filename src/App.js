54e4f8ef6694c22a3f0541b5147fe92c283c3d4a
Archakov Dennis <hello@archakov.im>
2022-10-06 21:43:08 +0600

    Интернет магазин react-sneakers

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
-import React from 'react';
-import axios from 'axios';
-import './styles.css';
+import Card from './component/Card';
+import Header from './component/Header';
+import Drawer from './component/Drawer';
 
 function App() {
-  const [users, setUsers] = React.useState([]);
-  const uploadFile = () => {
-    const fileELem = document.querySelector('#file');
-    const file = fileELem.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-
-    axios.post('https://localhost:9999', formData);
-  };
-
-  const gitUsers = async () => {
-    const { info } = await axios.get('https://6339640966857f698fb54345.mockapi.io/users');
-    setUsers(info);
-  };
-  const createProfile = () => {
-    const emailElement = document.querySelector('#Email');
-    const usernameElement = document.querySelector('#username');
-    const info = {
-      Email: emailElement.value,
-      name: usernameElement.value,
-    };
-    axios.post('https://6339640966857f698fb54345.mockapi.io/users', info);
-  };
   return (
-    <div className="App">
-      <ul>
-        {users.map((obj) => (
-          <li key={obj.id}>{obj.name}</li>
-        ))}
-      </ul>
-      <button onClick={gitUsers}>Получить список пользователя</button>
-      <br />
-      <br />
-      <hr />
-      <div>
-        <h4>Создайте аккаунт:</h4>
-        <input id="Email" placeholder="Email"></input>
-        <input id="username" placeholder="Имя"></input>
-        <button onClick={createProfile}>Отправить</button>
-      </div>
-      <br />
-      <br />
-      <hr />
-      <div>
-        <h4>Загрузка файла:</h4>
-        <input id="file" type="file" />
-        <button onClick={uploadFile}>Загрузить</button>
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>
+            <b>Все кроссовки</b>
+          </h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   );
