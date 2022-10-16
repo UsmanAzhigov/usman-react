1237051425680ba0a7fc68e17237944f588aa367
Archakov Dennis <hello@archakov.im>
2022-10-16 19:21:39 +0600

    React-sneakers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import 'macro-css';
 
-import { BrowserRouter } from 'react-router-dom';
-
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
